e0c97bd25cc354c5b767c24b542126abaa79226b
kotaiahandraju <a.kotaiah12@gmail.com>
2018-05-03 18:49:31 +0530

    adding order derails

--- src/main/java/com/aurospaces/neighbourhood/controller/BranchManagerController.java
+++ src/main/java/com/aurospaces/neighbourhood/controller/BranchManagerController.java
@@ -5,6 +5,7 @@ package com.aurospaces.neighbourhood.controller;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
@@ -14,21 +15,27 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.aurospaces.neighbourhood.bean.EmployeeBean;
+import com.aurospaces.neighbourhood.bean.ItemsBean;
 import com.aurospaces.neighbourhood.bean.LoginBean;
 import com.aurospaces.neighbourhood.db.dao.EmployeeDao;
+import com.aurospaces.neighbourhood.db.dao.LoginDao;
 import com.aurospaces.neighbourhood.db.dao.PaymentDao;
 import com.aurospaces.neighbourhood.util.SendSMS;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import CommonUtils.CommonUtils;
@@ -44,6 +51,9 @@ public class BranchManagerController {
 @Autowired PaymentDao paymentDao;
 @Autowired ServletContext objContext;
 @Autowired HttpSession session;
+@Autowired LoginDao loginDao;
+
+
 	@RequestMapping(value="/dealeraccountconfirm")
 	public String getDelarConfirmation(HttpServletRequest request){
 		List<Map<String,Object>> listOrderBeans =null;
@@ -114,23 +124,37 @@ public class BranchManagerController {
 	}
 	
 	@RequestMapping(value = "/authDetails")
-	public String authDetails( @ModelAttribute EmployeeBean employeeBean,	ModelMap model, HttpServletRequest request, HttpSession session,RedirectAttributes redirect) {
+	@ResponseBody public String authDetails( @ModelAttribute EmployeeBean employeeBean,	ModelMap model, HttpServletRequest request, HttpSession session,RedirectAttributes redirect) {
 		InputStream input = null;
 		String body = null;
 		 Properties prop = new Properties();
 		 LoginBean loginBean=null;
 		 boolean result=false;
 		 EmployeeBean checkEmpId=null;
+		 List<Map<String,Object>> listOrderBeans =null;
+			String json = null;
+			JSONObject jsonObject=new JSONObject();
 		try {
 			System.out.println("authDetailsauthDetailsauthDetails");
+			
+			LoginBean objuserBean = (LoginBean) session.getAttribute("cacheUserBean");
+			String password=CommonUtils.generatePIN();
 			loginBean=new LoginBean();
-			employeeBean.setPassword(CommonUtils.generatePIN());
-		System.out.println("login id"+CommonUtils.generatePIN());
+			employeeBean.setPassword(password);
+		loginBean.setUserName(employeeBean.getUsername());
+		
+		loginBean.setPassword(password);
+		loginBean.setBranchId(objuserBean.getBranchId());
+		loginBean.setRoleId("3");
+		loginBean.setStatus("1");
+		loginBean.setEmpId(String.valueOf(employeeBean.getId()));
 		checkEmpId=employeeDao.getByEmployeeId(employeeBean);
 		if(checkEmpId != null) {
-			loginBean.setPassword(CommonUtils.generatePIN());
 			result=employeeDao.updateUsernameAndPasswordInEmp(employeeBean.getId(), employeeBean.getUsername(), employeeBean.getPassword());
-			employeeDao.updateUsernameAndPasswordLogin(employeeBean.getId(), employeeBean.getUsername(), employeeBean.getPassword());
+			
+			loginDao.save(loginBean);
+			
+//			employeeDao.updateUsernameAndPasswordLogin(employeeBean.getId(), employeeBean.getUsername(), employeeBean.getPassword());
 			if(result) {
 				 String propertiespath = objContext.getRealPath("Resources" +File.separator+"DataBase.properties");
 					//String propertiespath = "C:\\PRO\\Database.properties";
@@ -154,6 +178,14 @@ public class BranchManagerController {
 				}*/
 				redirect.addFlashAttribute("msg", " Registered Successfully");
 				
+				ObjectMapper mapper = new ObjectMapper();
+				listOrderBeans = employeeDao.getAllDelarsConfirm("1",session);
+				if (listOrderBeans != null && listOrderBeans.size() > 0) {
+					jsonObject.put("allOrders1", listOrderBeans);
+				} else {
+					jsonObject.put("allOrders1", listOrderBeans);
+				}
+				
 			}
 		}
 //		
@@ -162,6 +194,59 @@ public class BranchManagerController {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		return body;
+		return String.valueOf(jsonObject);
 	}
+	 @RequestMapping(value = "/deleteDealer")
+		public @ResponseBody String deleteDealer(EmployeeBean  employeeBean, ModelMap model,
+				HttpServletRequest request, HttpSession session, BindingResult objBindingResult) {
+			System.out.println("deleteCylinder page...");
+			List<Map<String, Object>> listOrderBeans = null;
+			JSONObject jsonObj = new JSONObject();
+			ObjectMapper objectMapper = null;
+			String sJson = null;
+			boolean delete = false;
+			try {
+				if (employeeBean.getId() != 0 && employeeBean.getStatus() != "") {
+					delete = employeeDao.delete(employeeBean.getId(),
+							employeeBean.getStatus());
+					if (delete) {
+						jsonObj.put("message", "deleted");
+					} else {
+						jsonObj.put("message", "delete fail");
+					}
+				}
+
+				listOrderBeans = employeeDao.getAllDelarsConfirm("1",session);
+				if (listOrderBeans != null && listOrderBeans.size() > 0) {
+					jsonObj.put("allOrders1", listOrderBeans);
+				} else {
+					jsonObj.put("allOrders1", listOrderBeans);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+//				logger.error(e);
+//				logger.fatal("error in deleteRoomType class deleteEducation method  ");
+				jsonObj.put("message", "excetption" + e);
+				return String.valueOf(jsonObj);
+
+			}
+			return String.valueOf(jsonObj);
+		}
+	
+	
+	 @RequestMapping(value = "/inActiveDealer")
+		public @ResponseBody String inActiveDealer(@RequestParam("status") String status) throws JsonGenerationException, JsonMappingException, IOException {
+		 JSONObject jsonObject=new JSONObject();
+			List<Map<String, Object>> listOrderBeans = null;
+			String sJson="";
+			listOrderBeans = employeeDao.getAllDelarsConfirm(status,session);
+			if (listOrderBeans != null && listOrderBeans.size() > 0) {
+				jsonObject.put("allOrders1", listOrderBeans);
+			} else {
+				jsonObject.put("allOrders1", listOrderBeans);
+			}
+			
+			return String.valueOf(jsonObject);
+		}
+	
 }
---
+++ src/main/java/com/aurospaces/neighbourhood/controller/OrderPlacementController.java
@@ -0,0 +1,51 @@
+package com.aurospaces.neighbourhood.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aurospaces.neighbourhood.bean.ItemsBean;
+import com.aurospaces.neighbourhood.db.dao.EmployeeDao;
+import com.aurospaces.neighbourhood.db.dao.ItemsDao;
+import com.aurospaces.neighbourhood.db.dao.ProductnameDao;
+import com.fasterxml.jackson.databind.ObjectMapper;
+@Controller
+@RequestMapping(value="/admin")
+public class OrderPlacementController {
+	@Autowired EmployeeDao employeeDao;
+	@Autowired
+	ItemsDao  itemsDao;
+	@Autowired
+	ProductnameDao productnameDao;
+	@RequestMapping(value="/orderPlacement")
+	public String orderPlacement(HttpServletRequest request){
+		ObjectMapper objectMapper = null;
+		String sJson = null;
+		List<ItemsBean> listOrderBeans = null;
+		try{
+			System.out.println("orderPlacementorderPlacementorderPlacement");
+			listOrderBeans = itemsDao.getItems("1");
+			if (listOrderBeans != null && listOrderBeans.size() > 0) {
+				objectMapper = new ObjectMapper();
+				sJson = objectMapper.writeValueAsString(listOrderBeans);
+				request.setAttribute("allOrders1", sJson);
+					System.out.println(sJson);
+			} else {
+				objectMapper = new ObjectMapper();
+				sJson = objectMapper.writeValueAsString(listOrderBeans);
+				request.setAttribute("allOrders1", "''");
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "orderPlacement";
+		
+	}
+	
+}
